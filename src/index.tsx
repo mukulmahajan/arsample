2dcdb4992dfd72e4ee97a6ed28e923291b53d7d8
mukulmahajan <39019528+mukulmahajan@users.noreply.github.com>
2023-01-31 14:42:58 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
     <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
